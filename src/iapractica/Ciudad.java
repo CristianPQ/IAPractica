16328d71fe4cc76ff4fc3256dcb5cee856efac60
Cristian Pinto Quinto <cristianpintoquinto@gmail.com>
2015-10-14 23:19:47 +0200

    new class Ciudad

---
+++ src/iapractica/Ciudad.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package iapractica;
+
+/**
+ *
+ * @author Cristian
+ */
+public class Ciudad {
+    
+    private static int MAXPARADAS = 2;
+    private static int MAXBICISFURGONETA = 30;
+    //las furgonetas cargan solo en la estacion de origen
+    //varias furgonetas pueden dejar vicis en unamisma estacion
+    
+    //para evaluar la calidad de la solucion:
+        //--> Maximizacion de lo obtenido mediante traslados
+        //--> Minimizacion costes de transporte
+    
+    
+    
+    //Constructora
+    public Ciudad() {
+        
+    }
+    
+}
